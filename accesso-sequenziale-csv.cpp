dcd7fa6163903f5c4fb60556fd5613fc1da9be29
ilaria <ilariabianchi2010@gmail.com>
2026-05-19 08:41:14 +0200

    struttura iniziale

---
+++ accesso-sequenziale-csv.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void Aggiungi(){
+	
+}
+
+bool Modifica(){
+	
+}
+
+bool Cancella(){
+	
+}
+
+int main() {
+	int opzione;
+
+    do{
+        cout<<"APPLICAZIONE CSV\n1 - inserisci\n2 - visualizza\n3 - modifica\n4 - cancella\n0 - stop";
+        cout<<"\nopzione: ";
+        cin>>opzione;
+        
+        switch(opzione){
+        	
+        	case 1:{
+        		
+				break;
+			}
+			
+			case 2:{
+				
+				break;
+			}
+			
+			case 3:{
+				
+				break;
+			}
+			
+			case 4:{
+				
+				break;
+			}
+		}
+    }
+    while(opzione!=0);
+	
+	return 0;
+}
