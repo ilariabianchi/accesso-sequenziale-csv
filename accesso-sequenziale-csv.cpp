dd90ebaa7c5987da2fc72c56d0742c87163dac86
ilaria <bianchi.ilaria.studente@itispaleocapa.it>
2026-05-20 13:55:04 +0200

    funzione Caricadati e funzione Aggiungi che permette di aggiungere un elemento

--- accesso-sequenziale-csv.cpp
+++ accesso-sequenziale-csv.cpp
@@ -3,20 +3,78 @@
 #include <fstream>
 using namespace std;
 
-bool Aggiungi(){
+struct Location {
+    double latitudine1;
+    double longitudine1;
+};
+
+struct NumerazioneCivica {
+    string classe;
+    string descrizione;
+    string numero;
+    string subalterno;
+    string cap;
+    string istat;
+    double latitudine;
+    double longitudine;
+    Location posto;
+};
+
+bool CaricaDati(NumerazioneCivica v[], int righe_tot){
+	//apro il file
+    ifstream leggi("Comune_Bergamo_-_Numerazione_civica.csv");
+    //se il file non è aperto, ritorno falso
+    if(!leggi.is_open()) {
+        return false;
+    }
+    string riga, dati;
+    //salto la prima riga di intestazione
+    getline(leggi, riga);
+    int i=0;
+    //controllo che il ciclo si ripeta per il numero delle righe e che non sia alla fine del file
+    while (i<righe_tot && !leggi.eof()){
+    	//leggo il primo campo della riga e se siamo già alal fine del file esco dal ciclo
+        getline(leggi, v[i].classe, ',');
+        if(leggi.eof()){
+        	break;	
+		}
+		//leggo gli altri campi separati dalla virgola
+        getline(leggi, v[i].descrizione, ',');
+        getline(leggi, v[i].numero, ',');
+        getline(leggi, v[i].subalterno, ',');
+        getline(leggi, v[i].cap, ',');
+        getline(leggi, v[i].istat, ',');
+		//converto da stringa a double
+        getline(leggi, dati, ',');
+        v[i].latitudine=stod(dati);
+		//alla fine della riga si va a capo e la virgola non c'è più
+        getline(leggi, dati, '\n');
+        v[i].longitudine=stod(dati);
+		//copio gli stessi valori latitudine e longitudine in Location
+        v[i].posto.latitudine1=v[i].latitudine;
+        v[i].posto.longitudine1=v[i].longitudine;
+        i++;
+    }
+	//chiudo il file
+    leggi.close();
+    return true;
+}
+
+bool Aggiungi(string classe, string descrizione, string numero, string subalterno, string cap, string istat, double longit, double lat){
 	ifstream leggi("Comune_Bergamo_-_Numerazione_civica.csv");
-	ofstream scrivi("file2.csv");
+	ofstream scrivi("file2.csv", ios::app);
 	//copio nel nuovo file
 	string riga;
+	if(!scrivi.is_open()){
+		return false;
+	}
 	if(leggi.is_open()){
 		while(!leggi.eof()){
 			getline(leggi, riga);
 			scrivi<<riga<<endl;
 		}
     }
-    else{
-    	return false;
-	}
+    scrivi<<classe<<","<<descrizione<<","<<numero<<","<<subalterno<<","<<cap<<","<<istat<<","<<longit<<","<<lat<<",\"("<<longit<<","<<lat<<")\""<<endl;
 	return true;
 }
 
@@ -30,21 +88,56 @@ bool Aggiungi(){
 
 int main() {
 	int opzione;
-
+	NumerazioneCivica dati[1000];
+	NumerazioneCivica via;
+	int d=sizeof(dati)/sizeof(dati[0]);
+	
     do{
-        cout<<"APPLICAZIONE CSV\n1 - inserisci\n2 - visualizza\n3 - modifica\n4 - cancella\n0 - stop";
+        cout<<"APPLICAZIONE CSV\n1 - carica dati\n2 - visualizza file\n3 - modifica\n4 - cancella\n5 - inserisci\n0 - stop";
         cout<<"\nopzione: ";
         cin>>opzione;
         
         switch(opzione){
         	
         	case 1:{
+        		if(CaricaDati(dati, d)){
+                    cout<<"\ndati caricati\n\n";
+                }
+                //se i dati non vengono caricati si viene avvertiti
+                else{
+                    cout<<"\nfile non aperto\n\n";
+                }
         		
 				break;
 			}
 			
 			case 2:{
-				
+				cout<<"\ninserisci i dati della via che vuoi aggiungere:\n";
+				fflush(stdin);
+				cout<<"classe: ";
+				getline(cin, via.classe);
+				cout<<"descrizione: ";
+				getline(cin, via.descrizione);
+				cout<<"numero: ";
+				getline(cin, via.numero);
+				cout<<"subalterno: ";
+				getline(cin, via.subalterno);
+				cout<<"cap: ";
+				getline(cin, via.cap);
+				cout<<"istat: ";
+				getline(cin, via.istat);
+				cout<<"latitudine: ";
+				cin>>via.latitudine;
+				cout<<"longitudine: ";
+				cin>>via.longitudine;
+				fflush(stdin);
+				bool inserisci=Aggiungi(via.classe, via.descrizione, via.numero, via.subalterno, via.cap, via.istat, via.longitudine, via.latitudine);
+				if(inserisci){
+					cout<<"\nelemento aggiunto\n\n";
+				}
+				else{
+					cout<<"\nerrore nell'inserimento\n\n";
+				}
 				break;
 			}
 			
